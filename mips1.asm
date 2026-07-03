db87ef5c0a067e3715be5b3846d96b0db532b1a3
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-03 19:33:37 +0200

    Add simple MIPS 'Hello world' program
    Add mips1.asm: a minimal MIPS assembly example that defines myMessage in .data, loads its address into $a0, sets up $v0, and issues a syscall to print the message. Intended as a starter sample for MIPS/Spim/MARS usage.

---
+++ mips1.asm
@@ -0,0 +1,9 @@
+.data
+	myMessage: .asciiz "Hello world \n"
+
+.text
+	li $v0,50 #system please print out a text
+	la $a0, myMessage # what text? myMessage, ok I will load it in the register
+	syscall # now you have all the info, do it now
+	
+	
