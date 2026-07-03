ec5d141ed85311a8ead73bd412afd674a42447c2
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-03 19:53:46 +0200

    Rename mips1.asm; add dialog demo
    Rename mips1.asm to Youtube/AmellPeralta_Channel/03_01printString.asm. Add Youtube/AmellPeralta_Channel/03_02dialogCode50.asm containing a simple MIPS dialog example (syscall 50) that prompts and branches to yes/no/cancel message handlers.

---
+++ Youtube/AmellPeralta_Channel/03_02dialogCode50.asm
@@ -0,0 +1,29 @@
+.data
+	myMessage: .asciiz "Hello world \n"
+	mesSi: .asciiz "siii \n"
+	mesNo: .asciiz "noooo \n"
+	mesAnnulla: .asciiz "hai annullatooooo \n"
+
+.text
+	li $v0,50 # I want a dialog
+	la $a0, myMessage # question: 0 SI, 1 NO, 2 CANCEL
+	syscall
+	
+	li $v0, 4 # print function is 4
+	
+	beq $a0, 0, gotoSi
+	beq $a0, 1, gotoNo
+	beq $a0, 2, gotoAnnulla
+
+gotoSi:
+	la $a0, "ho cliccato si"
+	syscall
+	
+gotoNo:
+	la $a0 "ho cliccato no"
+	syscall
+
+gotoAnnulla:
+	la $a0 "ho cliccato annulla"
+	syscall
+	
