e319e23d21e7f8204bfa9d717fd0d1dc285ed5ab
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-04 03:37:35 +0200

    Add double-precision float printing example
    Add new MIPS assembly example (07_01printDouble.asm) demonstrating how to work with double-precision floating point numbers. The example loads two double values from memory, performs addition using the floating-point coprocessor, and prints the result using syscall code 3.

---
+++ Youtube/AmellPeralta_Channel/07_01printDouble.asm
@@ -0,0 +1,30 @@
+.data
+	first:   	.double 7.202
+	second: 	.double 3.67
+	
+	risultato: 	.double 0.0
+
+.text
+.globl main
+
+main:
+	la $t0, first 	#leggo indirizzo double nella ram
+	la $t1, second 	#leggo indirizzo double nella ram
+	
+	ldc1 $f4, 0($t0) # tutti gli $f tranne dispari e 0, 2,12,14... 0 e 2 sono come return $v e 12,14 come arguments $a
+	ldc1 $f6, 0($t1) # tutti gli $f tranne dispari e 0, 2,12,14... 0 e 2 sono come return $v e 12,14 come arguments $a
+	
+	add.d $f8, $f4, $f6  #sommiamo f4 e f6 che sono praticamente valori tradotti da indirizzo ram a messi in cpu coproc1 gia in double
+	
+	la $t3, risultato # indirizzo ram risultato
+	sdc1 $f8, 0($t3) # scrivo il risultato nella ram
+	
+	li $v0, 3 # write double code
+	ldc1 $f12, 0($t3) # uso indirizzo di risultato che ho già riempito con store
+	syscall
+	
+	j exit
+	
+exit:
+	li $v0, 10
+	syscall
