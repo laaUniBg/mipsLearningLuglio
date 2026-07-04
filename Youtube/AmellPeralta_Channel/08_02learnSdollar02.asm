6a8a14641cc1922d21cc0d891276963650fd91e0
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-04 20:36:58 +0200

    Add MIPS example aggiungi30 and main
    Adds new MIPS assembly file Youtube/AmellPeralta_Channel/08_02learnSdollar02.asm. Defines main (sets $s0=67, $s1=666), calls aggiungi30 which saves/restores $s0/$s1 on the stack, adds 30 to each, prints both values, and returns. Also removes a trailing newline in 08_02learnSdollar01.asm.

--- Youtube/AmellPeralta_Channel/08_02learnSdollar02.asm
+++ Youtube/AmellPeralta_Channel/08_02learnSdollar02.asm
@@ -1,6 +1,9 @@
 .data
 
 .text
+.globl main
+
+main:
 	li $s0, 67
 	li $s1, 666
 	
@@ -10,32 +13,21 @@
 	syscall
 	
 aggiungi30:
-	subi $sp, $sp, 4
+	addi $sp, $sp, -8
 	sw $s0, 0($sp)
+	sw $s1, 4($sp)
 	
 	addi $s0, $s0, 30
+	addi $s1, $s1, 30
+	
 	li $v0, 1
 	move $a0, $s0
 	syscall
+	move $a0, $s1
+	syscall
 	
 	lw $s0, 0($sp)
-	addi $sp, $sp, 4
-	
-	subi $sp, $sp, 4
-	sw $s1, 0($sp)
-	
-	addi $s1, $s1, 30
-	li $v0, 1
-	move $a0, $s1
-	
-	lw $s1, 0($sp)
-	addi $sp, $sp, 4
-	
-	jr $ra
-	
-	
-	
-	
-	
+	lw $s1, 4($sp)
+	addi $sp, $sp, 8
 
-	
+	jr $ra
