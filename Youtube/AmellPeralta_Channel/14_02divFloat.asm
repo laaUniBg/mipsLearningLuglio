3b641949d528cd8987da1fb9000575cbaf6de18a
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-05 14:11:49 +0200

    Add MIPS float division example
    Add 14_02divFloat.asm: a MIPS assembly example that converts two integers to single-precision floats (mtc1, cvt.s.w), performs floating-point division (div.s) and prints the result using syscall 2. Placed under Youtube/AmellPeralta_Channel as a learning example.

---
+++ Youtube/AmellPeralta_Channel/14_02divFloat.asm
@@ -0,0 +1,22 @@
+.data
+
+.text
+.globl main
+
+main:
+	li $t0, 30
+	li $t1, 8
+	
+	mtc1 $t0, $f4
+	mtc1 $t1, $f6
+	
+	cvt.s.w $f4, $f4
+	cvt.s.w $f6, $f6
+	
+	div.s $f8, $f4, $f6
+	
+	li $v0, 2
+	mov.d $f12, $f8
+	
+	syscall
+	
