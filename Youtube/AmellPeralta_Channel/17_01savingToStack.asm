ef2dc03847092c747f7d2f69a830be7e3ef6f43b
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-06 00:30:24 +0200

    Save registers; add increase and print subroutines
    Add newLine data and two subroutines: increaseMyRegister and printNewLine. main initializes $s0=10, calls increaseMyRegister (which saves $s0 and $ra on the stack, adds 30 to $s0, prints it, and restores registers), prints a newline, then prints $s0 and exits. Ensures $ra and $s0 are preserved across jal.
    
    Co-Authored-By: Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>

--- Youtube/AmellPeralta_Channel/17_01savingToStack.asm
+++ Youtube/AmellPeralta_Channel/17_01savingToStack.asm
@@ -1,13 +1,44 @@
 .data
-	
+	newLine: .asciiz "\n"
 .text
 .globl main
 
-# TODO
-
 main:
+	li $s0, 10
+	
+	jal increaseMyRegister
+	jal printNewLine
+	
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
 	j finishProgram
 	
 finishProgram:
 	li $v0, 10
 	syscall
+	
+increaseMyRegister:
+	addi $sp, $sp, -8
+	sw $s0, 0($sp)
+	sw $ra, 4($sp) # se uso una sottofunzione devo salvare $ra perche jal sovrascrive $ra subito
+	
+	addi $s0, $s0, 30
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	jal printNewLine
+	
+	lw $s0, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	
+	jr $ra
+	
+printNewLine:
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	jr $ra
