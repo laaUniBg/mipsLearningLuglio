a5cecba8357f61d8ab9c9f01101303a4775d685b
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-05 15:36:05 +0200

    Add MIPS example: functions with arg/return
    Add new MIPS assembly example (15_01proceduresFunctionsArgRet.asm) showing a procedure-style function (addNumbers) called with jal. Demonstrates passing arguments in $a1/$a2, returning result in $v1, using jr $ra to return, printing the result via syscall, and exiting the program. Includes a .data message (not used in this snippet).

---
+++ Youtube/AmellPeralta_Channel/15_01proceduresFunctionsArgRet.asm
@@ -0,0 +1,27 @@
+.data
+	message: .asciiz "Hi everybody. \nmy name is Anas."
+.text
+.globl main
+
+# procedure is function (like c++/js)
+
+main:
+	li $a1, 50
+	li $a2, 100	
+	
+	jal addNumbers
+	
+	li $v0, 1
+	move $a0, $v1
+	syscall
+	
+	j finishProgram
+
+
+addNumbers:
+	add $v1, $a1, $a2
+	jr $ra
+	
+finishProgram:
+	li $v0, 10
+	syscall
