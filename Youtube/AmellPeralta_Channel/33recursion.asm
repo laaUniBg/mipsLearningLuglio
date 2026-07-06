329d3a56f8f296eca9fb846daac9683a1188dcf0
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-07 00:36:56 +0200

    Add stack save/restore in findFactorial
    Introduce a simple prologue/epilogue in findFactorial: allocate 8 bytes on the stack and save $ra and $s0, then restore them and deallocate before returning. This preserves the return address and callee-saved register for recursive calls and prepares the function for a proper stack frame.
    
    Co-Authored-By: Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>

--- Youtube/AmellPeralta_Channel/33recursion.asm
+++ Youtube/AmellPeralta_Channel/33recursion.asm
@@ -35,6 +35,14 @@ main:
 	j finishProgram
 	
 findFactorial:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	addi $sp, $sp, 8
+	
 	jr $ra
 	
 writeString:
