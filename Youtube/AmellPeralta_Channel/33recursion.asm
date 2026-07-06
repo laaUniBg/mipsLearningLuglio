daebc3e0e0088a61878123b0947bf7e5b91f8cc7
laaUniBg <a.laaouatni@studenti.unibg.it>
2026-07-07 01:40:41 +0200

    Add recursive factorial implementation
    Implement recursive findFactorial in 33recursion.asm: create a stack frame (save/restore $ra and $s0), set up base case (n == 0 -> return 1), recurse with n-1, multiply result by saved n (using $s0) and return value in $v1. Also update the top TODO comment and minor whitespace fixes.
    
    Co-Authored-By: Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>

--- Youtube/AmellPeralta_Channel/33recursion.asm
+++ Youtube/AmellPeralta_Channel/33recursion.asm
@@ -8,7 +8,7 @@
 .globl main
 .globl findFactorial
 
-# TODO: finish the app, for now the app gets the number and it write it to the console
+# TODO: finish the app, for now is not working, only printing ONE but i will try to solve it
 
 main:
 	la $a1, promptMessage
@@ -33,18 +33,28 @@ main:
 	jal writeNumber
 	
 	j finishProgram
-	
+
 findFactorial:
 	addi $sp, $sp, -8
 	sw $ra, 0($sp)
 	sw $s0, 4($sp)
 	
+	li $v1, 1
+	# base case
+	beq $a1, $zero, factorialDone   # if (n == 0) return 1
+	
+	move $s0, $a1 # prendi argomento e lo salvi in $s0
+	sub $a1, $a1, 1 # n-1 e lo passo come parametro
+	jal findFactorial
+
+	mul $v1, $s0, $v1 # return v*fact
+
+factorialDone:
 	lw $ra, 0($sp)
 	lw $s0, 4($sp)
 	addi $sp, $sp, 8
-	
 	jr $ra
-	
+
 writeString:
 	li $v0, 4
 	move $a0, $a1
@@ -66,3 +76,4 @@ writeNumber:
 finishProgram:
 	li $v0,10
 	syscall
+	
